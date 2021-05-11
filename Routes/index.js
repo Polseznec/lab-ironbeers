812adbba88c224493582bc82b8ee35702e7f7d37
Pol Seznec <johndoe@example.com>
2021-05-11 09:14:42 +0200

    iteration 1/2 & 3

---
+++ Routes/index.js
@@ -0,0 +1,13 @@
+// var express = require("express");
+// var router = express.Router();
+
+
+// router.get("/", function (req, res, next) {
+//     // find all cats in db
+//     CatModel.find()
+//       .then((dbResult) => {
+//         // send all cats to the render function
+//         res.render("cats.hbs", { title: "cat's app", cats: dbResult });
+//       })
+//       .catch((dbErr) => next(dbErr));
+//   });
--- app.js
+++ app.js
@@ -22,4 +22,33 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+  res.render("beers.hbs")
+  .getBeers()
+  .then(beersFromApi => console.log('Beers from the database: ', beersFromApi))
+  .catch(error => console.log(error));
+});
+
+app.get('/randomBeer', (req, res) => {
+  res.render('randomBeer.hbs');
+});
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
+
+
